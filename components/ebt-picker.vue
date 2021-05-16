15c6004278d4c43b0e5e330d488d4d480fa4213f
Karl Lew <karl@firepick.org>
2021-05-16 05:23:53 -0400

    v1.0.27 ebt-picker

--- components/ebt-picker.vue
+++ components/ebt-picker.vue
@@ -1,15 +1,15 @@
 <template>
   <div :class="pickerClass"
-    @touchstart="onTouchStart($event)"
-    @touchmove="onTouchMove($event)"
-    @touchend="onTouchEnd($event)"
-    @touchcancel="onTouchCancel($event)"
     >
     <div v-for="(item,i) in items" :key="i"
       :data-index="i"
       :class="itemClass(i)"
       @mouseenter="onMouseEnter($event)"
       @mouseleave="onMouseLeave($event)"
+      @touchstart="onTouchStart($event)"
+      @touchmove="onTouchMove($event)"
+      @touchend="onTouchEnd($event)"
+      @touchcancel="onTouchCancel($event)"
       @click="onClick($event)"
     >{{i===iLabel ? label : bullet}}</div>
   </div>
@@ -44,7 +44,8 @@ export default {
       type: Array,
       default: ()=>{
         let result = [];
-        for (let i=100; i<=102; i++) {
+        let n = 20;
+        for (let i=100; i<100+n; i++) {
           result.push({
             label: `sn53.${i}/en`,
           });
@@ -67,11 +68,13 @@ export default {
   },
   methods:{
     touchesElement(touch, elt) {
-      let { clientX, clientY, radiusX=0, radiusY=0 } = touch;
+      if (touch == null) {
+        return false;
+      }
+      let { clientX, clientY } = touch;
       let r = elt.getBoundingClientRect();
-      let touchesX = r.x-radiusX <= clientX && clientX <= r.right+radiusX;
-      let touchesY = r.y-radiusY <= clientY && clientY <= r.bottom+radiusY;
-      console.log(`touchesElement`, touchesX, touchesY, touch, r, elt.dataset);
+      let touchesX = r.x <= clientX && clientX <= r.right;
+      let touchesY = r.y <= clientY && clientY <= r.bottom;
       return touchesX && touchesY;
     },
     touch(evt, propName="changedTouches") {
@@ -81,38 +84,38 @@ export default {
     touchTarget(evt, propName="changedTouches") {
       return evt.path[0];
     },
-    onTouchStart(evt) {
+    touchedIndex(evt) {
       let touch = this.touch(evt);
-      let target = evt.srcElement;
-      if (target) {
-        let parent = target.parentElement;
-        let rect = parent.getBoundingClientRect();
-        console.log(`onTouchStart`, rect, touch);
-      }
-      target && console.log(`onTouchStart`, target.dataset.index);
-    },
-    onTouchMove(evt) {
-      let target = this.touchTarget(evt);
-      target && console.log(`onTouchMove`, target.dataset.index);
-    },
-    onTouchCancel(evt) {
-      let target = this.touchTarget(evt);
-      target && console.log(`onTouchCancel`, target.dataset.index);
-    },
-    onTouchEnd(evt) {
-      let { srcElement } = evt;
-      let parent = srcElement.parentElement;
-      let rect = parent.getBoundingClientRect();
-      let touch = this.touch(evt);
-      if (touch) {
-        let { clientX, clientY, radiusX=0, radiusY=0 } = touch;
-        if (rect.x-radiusX <= clientX && clientX <= rect.right+radiusX &&
-            rect.y-radiusY <= clientY && clientY <= rect.bottom+radiusY) {
-          console.log(`onTouchEnd in`, evt);
-        } else {
-          console.log(`onTouchEnd out`, evt);
+      let { children:siblings } = evt.target.parentElement;
+      let index = -1;
+      for (let i=0; i<siblings.length; i++) {
+        let elt = siblings[i];
+        if (this.touchesElement(touch, elt)) {
+          index = elt.dataset.index;
         }
       }
+      return index;
+    },
+    onTouchStart(evt) {
+      evt.stopPropagation();
+      let index = this.touchedIndex(evt);
+      Vue.set(this, "iHover", index);
+      console.log(`onTouchStart`, {index});
+    },
+    onTouchMove(evt) {
+      evt.stopPropagation();
+      let index = this.touchedIndex(evt);
+      Vue.set(this, "iHover", index);
+    },
+    onTouchCancel(evt) {
+      evt.stopPropagation();
+      Vue.set(this, 'iHover', -1);
+      console.log(`onTouchCancel`, evt);
+    },
+    onTouchEnd(evt) {
+      evt.stopPropagation();
+      this.onClick(evt);
+      Vue.set(this, 'iHover', -1);
     },
     onMouseEnter(evt) {
       evt.stopPropagation();
@@ -131,11 +134,9 @@ export default {
       console.debug(`onMouseLeave`, {index});
     },
     onClick(evt) {
-      let { target } = evt;
-      let { index } = target && target.dataset;
       let { iHover, items, iLabel, label } = this;
-      if (index != null) {
-        console.log(`onClick`, index, iHover, label, evt);
+      if (iHover >= 0) {
+        console.log(`onClick`, iHover, iLabel, label, evt);
       }
     },
     itemClass(i) {
@@ -166,10 +167,13 @@ export default {
         : items.length + labelIndex;
     },
     pickerClass() {
-      let { iLabel } = this;
-      return iLabel === 0
-        ? "ebt-picker ebt-picker-left"
-        : "ebt-picker ebt-picker-right";
+      let { iLabel, iHover } = this;
+      let classes = ['ebt-picker'];
+      classes.push( iLabel === 0
+        ? "ebt-picker-left"
+        : "ebt-picker-right");
+      iHover >= 0 && classes.push('ebt-picker-hover');
+      return classes.join(' ');
     },
     label() {
       let { iHover, iLabel, items } = this;
@@ -180,53 +184,4 @@ export default {
 }
 </script>
 <style>
-.ebt-picker {
-  display: flex;
-  flex-flow: row nowrap;
-  align-items: center;
-  width: 33%;
-  overflow: hidden;
-}
-.ebt-picker:hover {
-  color: var(--ebt-focus-color-light) !important;
-}
-.ebt-picker-left {
-  justify-content: flex-start;
-}
-.ebt-picker-right {
-  justify-content: flex-end;
-}
-.ebt-picker > div {
-  cursor: pointer;
-}
-.ebt-picker-item0 {
-  opacity: 1;
-}
-.ebt-picker-item1 {
-  opacity: 0.6;
-}
-.ebt-picker-item2 {
-  opacity: 0.2;
-}
-.ebt-picker-itemN {
-  opacity: 0.1;
-}
-.ebt-picker:hover .ebt-picker-item-label {
-  font-weight: 700;
-  text-decoration: underline;
-}
-.ebt-picker-item-label {
-  padding-left: 0.3em;
-  padding-right: 0.3em;
-  border-top: 1pt solid rgba(0,0,0,0);
-  border-bottom: 1pt solid rgba(0,0,0,0);
-}
-.ebt-picker-item-inactive {
-  opacity: 0;
-}
-.ebt-picker:hover .ebt-picker-item0,
-.ebt-picker:hover .ebt-picker-item1,
-.ebt-picker:hover .ebt-picker-item2{
-  text-decoration: underline;
-}
 </style>
